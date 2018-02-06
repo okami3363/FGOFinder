d0e9789cd7dccd0751eaba7ffb5fa23ab42a4702
okami3363 <okami3363@gmail.com>
2018-02-06 15:19:39 +0800

    Create tabBarController

--- FGOFinder/FGOFinder/AppDelegate.swift
+++ FGOFinder/FGOFinder/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let tabBarController: UITabBarController = UITabBarController()
+        let firstViewController = FirstViewController()
+        let secondViewController = SecondViewController();
+        
+        tabBarController.viewControllers = [firstViewController, secondViewController];
         return true
     }
 
