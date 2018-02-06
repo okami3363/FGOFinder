14d79f9bbbcc71ab54ba4c06b9aea06a8521bc62
okami3363 <okami3363@gmail.com>
2018-02-06 17:53:23 +0800

    UIWindow bug fix

--- FGOFinder/FGOFinder/AppDelegate.swift
+++ FGOFinder/FGOFinder/AppDelegate.swift
@@ -17,11 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let tabBarController: UITabBarController = UITabBarController()
+        self.window = UIWindow(frame: UIScreen.main.bounds);
+        let tabBarController = UITabBarController()
         let firstViewController = FirstViewController()
-        let secondViewController = SecondViewController();
+        firstViewController.title = "Servant"
+        let secondViewController = SecondViewController()
+        secondViewController.title = "概念禮裝"
+        tabBarController.viewControllers = [firstViewController, secondViewController]
+        self.window?.rootViewController = tabBarController
         
-        tabBarController.viewControllers = [firstViewController, secondViewController];
+        self.window?.makeKeyAndVisible()
         return true
     }
 
